1ad1923a34c46bb1e323fb9761cc4c767809bb28
gwak2837 <gwak2837@gmail.com>
2025-06-30 16:55:50 +0900

    format

--- js/plugins/ButtonPicture.js
+++ js/plugins/ButtonPicture.js
@@ -69,38 +69,35 @@
  */
 
 (() => {
-    const pluginName = "ButtonPicture";
+  const pluginName = "ButtonPicture";
 
-    PluginManager.registerCommand(pluginName, "set", args => {
-        const pictureId = Number(args.pictureId);
-        const commonEventId = Number(args.commonEventId);
-        const picture = $gameScreen.picture(pictureId);
-        if (picture) {
-            picture.mzkp_commonEventId = commonEventId;
-        }
-    });
+  PluginManager.registerCommand(pluginName, "set", (args) => {
+    const pictureId = Number(args.pictureId);
+    const commonEventId = Number(args.commonEventId);
+    const picture = $gameScreen.picture(pictureId);
+    if (picture) {
+      picture.mzkp_commonEventId = commonEventId;
+    }
+  });
 
-    Sprite_Picture.prototype.isClickEnabled = function() {
-        const picture = this.picture();
-        return picture && picture.mzkp_commonEventId && !$gameMessage.isBusy();
-    };
+  Sprite_Picture.prototype.isClickEnabled = function () {
+    const picture = this.picture();
+    return picture && picture.mzkp_commonEventId && !$gameMessage.isBusy();
+  };
 
-    Sprite_Picture.prototype.onClick = function() {
-        $gameTemp.reserveCommonEvent(this.picture().mzkp_commonEventId);
-    };
+  Sprite_Picture.prototype.onClick = function () {
+    $gameTemp.reserveCommonEvent(this.picture().mzkp_commonEventId);
+  };
 
-    Spriteset_Base.prototype.mzkp_isAnyPicturePressed = function() {
-        return this._pictureContainer.children.some(sprite =>
-            sprite.isPressed()
-        );
-    };
+  Spriteset_Base.prototype.mzkp_isAnyPicturePressed = function () {
+    return this._pictureContainer.children.some((sprite) => sprite.isPressed());
+  };
 
-    const _Scene_Map_isAnyButtonPressed =
-        Scene_Map.prototype.isAnyButtonPressed;
-    Scene_Map.prototype.isAnyButtonPressed = function() {
-        return (
-            _Scene_Map_isAnyButtonPressed.apply(this, arguments) ||
-            this._spriteset.mzkp_isAnyPicturePressed()
-        );
-    };
+  const _Scene_Map_isAnyButtonPressed = Scene_Map.prototype.isAnyButtonPressed;
+  Scene_Map.prototype.isAnyButtonPressed = function () {
+    return (
+      _Scene_Map_isAnyButtonPressed.apply(this, arguments) ||
+      this._spriteset.mzkp_isAnyPicturePressed()
+    );
+  };
 })();
